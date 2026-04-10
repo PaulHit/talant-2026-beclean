e7570833813920de83ded60e441ed97b36a7c845
PaulHit <hiticaspaul@gmail.com>
2026-04-10 12:32:42 +0300

    analytics + speed insights

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Playfair_Display, Nunito } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const playfair = Playfair_Display({
   subsets: ['latin'],
